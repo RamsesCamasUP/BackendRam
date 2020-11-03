69985d9cd5747f2844068dd4021a086632021901
Ramses <machiniram@gmail.com>
2020-11-02 19:01:46 -0600

    Add dashboard at admin v2

--- Dashboard/models.py
+++ Dashboard/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class DashboardModel(models.Model):
-    nombre_com = models.TextField(max_length=50)
-    edad = models.TextField(max_length=10)
-    correo = models.TextField(max_length=50)
+    nombre_com = models.CharField(max_length=50)
+    edad = models.CharField(max_length=10)
+    correo = models.CharField(max_length=50)
 
     def __str__(self):
         return self.nombre_com
