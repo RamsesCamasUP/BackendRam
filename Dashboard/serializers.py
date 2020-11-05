df8bc8ea23b4cc97ae4bde5b5c522ef482e31511
Ramses <machiniram@gmail.com>
2020-11-05 11:41:58 -0600

    Last changes to get data

--- Dashboard/serializers.py
+++ Dashboard/serializers.py
@@ -4,4 +4,4 @@ from .models import DashboardModel
 class DashboardSerializer(serializers.ModelSerializer):
     class Meta:
         model = DashboardModel
-        fields =('nombre_com','edad','correo')
+        fields =('__all__')
