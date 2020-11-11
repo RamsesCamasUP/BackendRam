3f5cb17b80259c01280e6a017cc4bd19f39139e2
Ramses <machiniram@gmail.com>
2020-11-11 08:42:02 -0600

    Deploy Heroku

--- Login/urls.py
+++ Login/urls.py
@@ -5,6 +5,7 @@ from rest_framework import routers, serializers, viewsets
 from Login.views import CustomAuthToken
 
 urlpatterns = [
-
+    re_path(r'^rest-auth/', include('rest_auth.urls')),
+    re_path(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
     re_path(r'^', CustomAuthToken.as_view()),
 ]
