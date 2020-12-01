1da0ba06d15bc7d3a4d9a32919217558db9c12c6
Ramses <machiniram@gmail.com>
2020-12-01 14:15:09 -0600

    Masked ip for deploy

--- TrayectoriaBack/settings.py
+++ TrayectoriaBack/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',default=False,cast=bool)
 
-ALLOWED_HOSTS = ['18.204.5.204']
+ALLOWED_HOSTS = ['18.204.5.204','http://web-ramses.ddns.net/']
 
 
 # Application definition
